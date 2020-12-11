f2476b16e33f19b45a0dcb8ea3ad794d809df28e
Radmaster5000 <phil.radford5000@gmail.com>
2020-12-11 16:05:42 +0000

    literally replaced the need for any python coding with a single bash command

--- textify.sh
+++ textify.sh
@@ -1,3 +1,2 @@
-playlist=$1
-echo $1
-sed 's/Users//' Workout.m3u8 > Workout.txt
+
+sed 's+/Users/Radmaster5000/Music/iTunes/iTunes Media/Music/++g' /Users/Radmaster5000/Desktop/Workout.m3u8 > Workout.m3u8
