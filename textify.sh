d66c98ed05e2d0438fed5257d02dd828c83d7552
Radmaster5000 <phil.radford5000@gmail.com>
2020-12-13 23:07:11 +0000

    worked on looping through files in bash to list playlists

--- textify.sh
+++ textify.sh
@@ -1,2 +1,4 @@
-
-sed 's+/Users/Radmaster5000/Music/iTunes/iTunes Media/Music/++g' /Users/Radmaster5000/Desktop/Workout.m3u8 > Workout.m3u8
+name=$1
+echo $name
+for FILE in /Users/Radmaster5000/Desktop/iTunesPlaylists/*; do echo $FILE; done
+sed 's+/Users/Radmaster5000/Music/iTunes/iTunes Media/Music/++g' /Users/Radmaster5000/Desktop/${name}.m3u8 > /Users/Radmaster5000/Desktop/WalkmanPlaylists/${name}.m3u8
