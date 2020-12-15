22c3ef8f0e1b53007b39db197351f2dfd772ffbe
Radmaster5000 <phil.radford5000@gmail.com>
2020-12-15 00:29:59 +0000

    Reads the playlists in one file and copies them to another file with altered paths but runs into issues with spaces in playlist names

--- textify.sh
+++ textify.sh
@@ -1,4 +1,17 @@
-name=$1
-echo $name
-for FILE in /Users/Radmaster5000/Desktop/iTunesPlaylists/*; do echo $FILE; done
-sed 's+/Users/Radmaster5000/Music/iTunes/iTunes Media/Music/++g' /Users/Radmaster5000/Desktop/${name}.m3u8 > /Users/Radmaster5000/Desktop/WalkmanPlaylists/${name}.m3u8
+
+
+arr=(/Users/Radmaster5000/Desktop/iTunesPlaylists/*)
+prefix="/Users/Radmaster5000/Desktop/iTunesPlaylists/"
+suffix=".m3u8"
+
+#for FILE in /Users/Radmaster5000/Desktop/iTunesPlaylists/*; do echo $FILE; done
+
+for ((i=0; i<${#arr[@]}; i++)); do
+	listName=${arr[$i]}
+	newListName=${listName#"$prefix"}
+	newListName=${newListName%"$suffix"}
+	echo $newListName
+	sed 's+/Users/Radmaster5000/Music/iTunes/iTunes Media/Music/++g' ${arr[$i]} > /Users/Radmaster5000/Desktop/WalkmanPlaylists/${newListName}.m3u8
+done
+
+
