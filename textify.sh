5e96cdd51a59a07b4877399d5a98f66901e7be25
Radmaster5000 <phil.radford5000@gmail.com>
2020-12-16 21:50:50 +0000

    acknowledges files with spaces and creates playlists replacing the titles spaces with underscores but has issues copying them

--- textify.sh
+++ textify.sh
@@ -7,11 +7,16 @@ suffix=".m3u8"
 #for FILE in /Users/Radmaster5000/Desktop/iTunesPlaylists/*; do echo $FILE; done
 
 for ((i=0; i<${#arr[@]}; i++)); do
-	listName=${arr[$i]}
+	if [[ ${arr[$i]} = *[[:space:]]* ]]; then
+			listName=${arr[$i]// /_}
+	else
+		listName=${arr[$i]}
+	fi
+
 	newListName=${listName#"$prefix"}
 	newListName=${newListName%"$suffix"}
-	echo $newListName
 	sed 's+/Users/Radmaster5000/Music/iTunes/iTunes Media/Music/++g' ${arr[$i]} > /Users/Radmaster5000/Desktop/WalkmanPlaylists/${newListName}.m3u8
+	echo $newListName created
 done
 
 
